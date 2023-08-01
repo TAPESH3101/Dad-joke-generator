74c0585bdd573a7a2d509ddd242c576f963acaa5
Tapesh <tapeshchahar2002@gmail.com>
2023-08-01 08:13:41 +0530

    final commit

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ const options = {
   },
 };
 
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+
+async function getJoke() {
+  try {
+    jokeEl.innerText = "Updating...";
+    btnEl.disabled = true;
+    btnEl.innerText = "Loading...";
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
+
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+
+    jokeEl.innerText = data[0].joke;
+  } catch (error) {
+    jokeEl.innerText = "An error happened, try again later";
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+    console.log(error);
+  }
+}
+
+btnEl.addEventListener("click", getJoke);
