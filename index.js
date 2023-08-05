e613a4f356655a5c03173fb8eb133313337409b7
Tapesh <tapeshchahar2002@gmail.com>
2023-08-05 08:17:08 +0530

    final commit

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const btnEl = document.getElementById("btn");
 const jokeEl = document.getElementById("joke");
 
 const apiKey = "4kqGcJx8uDXo3XIskcbzokAz7rN8nWJs3PL9Mcll";
-
 const options = {
   method: "GET",
   headers: {
